a22afbfea29d322c4af2d2819d08341d886a39b1
Валерия Майорова <majorova.vs@edu.spbstu.ru>
2020-03-22 01:09:01 +0300

    Settings added

--- lab4/Program.cs
+++ lab4/Program.cs
@@ -11,6 +11,42 @@ namespace lab4
     {
         void Paint();
     }
+
+    interface ITimeZone
+    {
+        void Set();
+    }
+
+    interface ILanguage
+    {
+        void Set();
+    }
+
+    interface IRegion
+    {
+        void Set();
+    }
+    //Задание клавиш переключения раскладки клавиатуры 
+    interface ISwkeyblay
+    {
+        void Set();
+    }
+
+    interface IUser
+    {
+        void Create();
+    }
+    //Настройка устройств
+    interface IDevice
+    {
+        void Set();
+    }
+
+    //Настройка жёсткого диска (разбивание на разделы)
+    interface IHardDisk
+    {
+        void Setup();
+    }
     /**
      * Абстрактная фабрика знает обо всех абстрактных типах
      * продуктов.
@@ -18,6 +54,48 @@ namespace lab4
     interface IGUIFactory {
         IButton CreateButton();
         ICheckBox CreateCheckbox();
+        ITimeZone SetTimeZone();
+        ILanguage SetLanguage();
+        IRegion SetRegion();
+        ISwkeyblay SetSwKeyL();
+        IUser CreateUser();
+        IDevice SetDevice();
+        IHardDisk SetupHardDisk();
+
+    }
+    class WinHardDisk : IHardDisk
+    {
+        public void Setup() { }
+    }
+    class WinDevice : IDevice
+    {
+        public void Set() { }
+    }
+    class WinUser : IUser
+    {
+        public void Create() { Console.WriteLine("User is created"); }
+    }
+    class WinSwKeybl : ISwkeyblay
+    {
+        public void Set() { }
+    }
+    class WinLanguage : ILanguage
+    {
+        public void Set()
+        {
+
+        }
+    }
+    class WinRegion : IRegion
+    {
+        public void Set() {}
+    }
+    class WinTimeZone : ITimeZone
+    {
+        public void Set()
+        {
+            Console.WriteLine("Windows time zone setting");
+        }
     }
     class WinButton : IButton
     {
@@ -35,6 +113,38 @@ namespace lab4
         }
     }
 
+    class MacHardDisk : IHardDisk
+    {
+        public void Setup() { }
+    }
+    class MacDevice : IDevice
+    {
+        public void Set() { }
+    }
+    class MacUser : IUser
+    {
+        public void Create() { Console.WriteLine("User is created"); }
+    }
+    class MacSwKeybl : ISwkeyblay
+    {
+        public void Set() { }
+    }
+    class MacLanguage : ILanguage
+    {
+        public void Set() { }
+    }
+    class MacRegion : IRegion
+    {
+        public void Set() { }
+    }
+    class MacTimeZone : ITimeZone
+    {
+        public void Set()
+        {
+            Console.WriteLine("MacOS time zone setting");
+        }
+    }
+
     class MacButton : IButton
     {
         public void Paint()
@@ -48,6 +158,38 @@ namespace lab4
         public void Paint()
         {
             Console.WriteLine("mac style checkbox");
+        }
+    }
+
+    class LinHardDisk : IHardDisk
+    {
+        public void Setup() { }
+    }
+    class LinDevice : IDevice
+    {
+        public void Set() { }
+    }
+    class LinUser : IUser
+    {
+        public void Create() { Console.WriteLine("User is created"); }
+    }
+    class LinSwKeybl : ISwkeyblay
+    {
+        public void Set() { }
+    }
+    class LinLanguage : ILanguage
+    {
+        public void Set() { }
+    }
+    class LinRegion : IRegion
+    {
+        public void Set() { }
+    }
+    class LinTimeZone : ITimeZone
+    {
+        public void Set()
+        {
+            Console.WriteLine("Linux time zone setting");
         }
     }
     class LinButton : IButton
@@ -79,6 +221,34 @@ namespace lab4
         {
             return new WinCheckbox();
         }
+        public ILanguage SetLanguage()
+        {
+            return new WinLanguage();
+        }
+        public ITimeZone SetTimeZone()
+        {
+            return new WinTimeZone();
+        }
+        public IRegion SetRegion()
+        {
+            return new WinRegion();
+        }
+        public ISwkeyblay SetSwKeyL()
+        {
+            return new WinSwKeybl();
+        }
+        public IUser CreateUser()
+        {
+            return new WinUser();
+        }
+        public IDevice SetDevice()
+        {
+            return new WinDevice();
+        }
+        public IHardDisk SetupHardDisk()
+        {
+            return new WinHardDisk();
+        }
     }
     class MacFactory : IGUIFactory
     {
@@ -89,6 +259,34 @@ namespace lab4
         public ICheckBox CreateCheckbox()
         {
             return new MacCheckbox();
+        }
+        public ILanguage SetLanguage()
+        {
+            return new MacLanguage();
+        }
+        public ITimeZone SetTimeZone()
+        {
+            return new MacTimeZone();
+        }
+        public IRegion SetRegion()
+        {
+            return new MacRegion();
+        }
+        public ISwkeyblay SetSwKeyL()
+        {
+            return new MacSwKeybl();
+        }
+        public IUser CreateUser()
+        {
+            return new MacUser();
+        }
+        public IDevice SetDevice()
+        {
+            return new MacDevice();
+        }
+        public IHardDisk SetupHardDisk()
+        {
+            return new MacHardDisk();
         }
     }
     class LinFactory : IGUIFactory
@@ -101,6 +299,34 @@ namespace lab4
         {
             return new LinCheckbox();
         }
+        public ILanguage SetLanguage()
+        {
+            return new LinLanguage();
+        }
+        public ITimeZone SetTimeZone()
+        {
+            return new LinTimeZone();
+        }
+        public IRegion SetRegion()
+        {
+            return new LinRegion();
+        }
+        public ISwkeyblay SetSwKeyL()
+        {
+            return new LinSwKeybl();
+        }
+        public IUser CreateUser()
+        {
+            return new LinUser();
+        }
+        public IDevice SetDevice()
+        {
+            return new LinDevice();
+        }
+        public IHardDisk SetupHardDisk()
+        {
+            return new LinHardDisk();
+        }
     }
 
     class Application
@@ -108,6 +334,13 @@ namespace lab4
         private IGUIFactory Factory { get; set; }
         private IButton Button { get; set; }
         private ICheckBox CheckBox { get; set; }
+        private ILanguage Language { get; set; }
+        private ITimeZone TimeZone { get; set; }
+        private IRegion Region { get; set; }
+        private ISwkeyblay Swkeyblay { get; set; }
+        private IUser User { get; set; }
+        private IDevice Device { get; set; }
+        private IHardDisk HardDisk { get; set; }
         public Application(IGUIFactory factory)
         {
             this.Factory = factory;
@@ -118,8 +351,28 @@ namespace lab4
             this.CheckBox = Factory.CreateCheckbox();
         }
 
+        public void RegionSetting()
+        {
+            this.Region = Factory.SetRegion();
+            this.TimeZone = Factory.SetTimeZone();
+            this.Language = Factory.SetLanguage();
+        }
+
+        public void CreateUser()
+        {
+            this.User = Factory.CreateUser();
+        }
+
+        public void DeviceSetting()
+        {
+            this.Device = Factory.SetDevice();
+            this.HardDisk = Factory.SetupHardDisk();
+            this.Swkeyblay = Factory.SetSwKeyL();
+        }
+
         public void Paint()
         {
+            User.Create();
             Button.Paint();
             CheckBox.Paint();
         }
@@ -146,6 +399,9 @@ namespace lab4
             else throw new Exception("Uknown OS");
 
             Application application = new Application(factory);
+            application.CreateUser();
+            application.RegionSetting();
+            application.DeviceSetting();
             application.CreateUI();
             application.Paint();
         }
